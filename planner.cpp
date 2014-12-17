484d6b7621455c1adf882e247f7930ded174acb1
Federico Ferri <federico.ferri.it@gmail.com>
2014-12-17 20:58:56 +0100

    switch to ompl-opende code

--- planner.cpp
+++ planner.cpp
@@ -7,78 +7,243 @@
 //
 
 #include "planner.h"
+#include <ompl/extensions/opende/OpenDESimpleSetup.h>
+#include <ompl/base/goals/GoalRegion.h>
+#include <ompl/config.h>
 #include <iostream>
+#include <ode/ode.h>
 
 namespace ob = ompl::base;
-namespace oc = ompl::control;
 namespace og = ompl::geometric;
+namespace oc = ompl::control;
 
-static bool isStateValid(const ob::State *state) {
-    return true;
-}
+/// @cond IGNORE
 
-static void propagateState(const ob::State *state, const oc::Control *control, const double duration, ob::State* newState) {
-}
+class RigidBodyEnvironment : public oc::OpenDEEnvironment {
+public:
+    RigidBodyEnvironment(void) : oc::OpenDEEnvironment() {
+        createWorld();
+    }
+    
+    virtual ~RigidBodyEnvironment(void) {
+        destroyWorld();
+    }
+    
+    /**************************************************
+     * Implementation of functions needed by planning *
+     **************************************************/
+    
+    virtual unsigned int getControlDimension(void) const {
+        return 3;
+    }
+    
+    virtual void getControlBounds(std::vector<double> &lower, std::vector<double> &upper) const {
+        static double maxForce = 0.2;
+        
+        lower.resize(3);
+        lower[0] = -maxForce;
+        lower[1] = -maxForce;
+        lower[2] = -maxForce;
+        
+        upper.resize(3);
+        upper[0] = maxForce;
+        upper[1] = maxForce;
+        upper[2] = maxForce;
+    }
+    
+    virtual void applyControl(const double *control) const {
+        dBodyAddForce(boxBody, control[0], control[1], control[2]);
+    }
+    
+    virtual bool isValidCollision(dGeomID g1, dGeomID g2, const dContact& contact) const {
+        return false;
+    }
+    
+    virtual void setupContact(dGeomID g1, dGeomID g2, dContact &contact) const {
+        contact.surface.mode = dContactSoftCFM | dContactApprox1;
+        contact.surface.mu = 0.9;
+        contact.surface.soft_cfm = 0.2;
+    }
+    
+    /**************************************************/
+    
+    
+    // OMPL does not require this function here; we implement it here
+    // for convenience. This function is only OpenDE code to create a
+    // simulation environment. At the end of the function, there is a
+    // call to setPlanningParameters(), which configures members of
+    // the base class needed by planners.
+    void createWorld();
+    
+    // Clear all OpenDE objects
+    void destroyWorld();
+    
+    // Set parameters needed by the base class (such as the bodies
+    // that make up to state of the system we are planning for)
+    void setPlanningParameters();
+    
+    // the simulation world
+    dWorldID bodyWorld;
+    
+    // the space for all objects
+    dSpaceID space;
+    
+    // the car mass
+    dMass    m;
+    
+    // the body geom
+    dGeomID  boxGeom;
+    
+    // the body
+    dBodyID  boxBody;
+    
+};
+
+// Define the goal we want to reach
+class RigidBodyGoal : public ob::GoalRegion {
+public:
+    RigidBodyGoal(const ob::SpaceInformationPtr &si) : ob::GoalRegion(si) {
+        threshold_ = 0.5;
+    }
+    
+    virtual double distanceGoal(const ob::State *st) const {
+        const double *pos = st->as<oc::OpenDEStateSpace::StateType>()->getBodyPosition(0);
+        double dx = fabs(pos[0] - 30);
+        double dy = fabs(pos[1] - 55);
+        double dz = fabs(pos[2] - 35);
+        return sqrt(dx * dx + dy * dy + dz * dz);
+    }
+};
+
+// Define how we project a state
+class RigidBodyStateProjectionEvaluator : public ob::ProjectionEvaluator {
+public:
+    RigidBodyStateProjectionEvaluator(const ob::StateSpace *space) : ob::ProjectionEvaluator(space) {
+    }
+    
+    virtual unsigned int getDimension(void) const {
+        return 3;
+    }
+    
+    virtual void defaultCellSizes() {
+        cellSizes_.resize(3);
+        cellSizes_[0] = 1;
+        cellSizes_[1] = 1;
+        cellSizes_[2] = 1;
+    }
+    
+    virtual void project(const ob::State *state, ob::EuclideanProjection &projection) const {
+        const double *pos = state->as<oc::OpenDEStateSpace::StateType>()->getBodyPosition(0);
+        projection[0] = pos[0];
+        projection[1] = pos[1];
+        projection[2] = pos[2];
+    }
+};
+
+// Define our own space, to include a distance function we want and register a default projection
+class RigidBodyStateSpace : public oc::OpenDEStateSpace {
+public:
+    RigidBodyStateSpace(const oc::OpenDEEnvironmentPtr &env) : oc::OpenDEStateSpace(env) {
+    }
+    
+    virtual double distance(const ob::State *s1, const ob::State *s2) const {
+        const double *p1 = s1->as<oc::OpenDEStateSpace::StateType>()->getBodyPosition(0);
+        const double *p2 = s2->as<oc::OpenDEStateSpace::StateType>()->getBodyPosition(0);
+        double dx = fabs(p1[0] - p2[0]);
+        double dy = fabs(p1[1] - p2[1]);
+        double dz = fabs(p1[2] - p2[2]);
+        return sqrt(dx * dx + dy * dy + dz * dz);
+    }
+    
+    virtual void registerProjections() {
+        registerDefaultProjection(ob::ProjectionEvaluatorPtr(new RigidBodyStateProjectionEvaluator(this)));
+    }
+};
+
+/// @endcond
 
 void plan() {
-    // construct the state space we are planning in
-    ob::StateSpacePtr space(new ob::SE3StateSpace());
+    // create the OpenDE environment
+    oc::OpenDEEnvironmentPtr env(new RigidBodyEnvironment());
+    
+    // create the state space and the control space for planning
+    RigidBodyStateSpace *stateSpace = new RigidBodyStateSpace(env);
+    ob::StateSpacePtr stateSpacePtr = ob::StateSpacePtr(stateSpace);
+    
+    // this will take care of setting a proper collision checker and the starting state for the planner as the initial OpenDE state
+    oc::OpenDESimpleSetup ss(stateSpacePtr);
+    
+    // set the goal we would like to reach
+    ss.setGoal(ob::GoalPtr(new RigidBodyGoal(ss.getSpaceInformation())));
+    
     ob::RealVectorBounds bounds(3);
-    bounds.setLow(-1);
-    bounds.setHigh(1);
-    space->as<ob::SE3StateSpace>()->setBounds(bounds);
-
-    //ob::SpaceInformationPtr si(new ob::SpaceInformation(space));
-    //si->setStateValidityChecker(boost::bind(&isStateValid, _1));
-    //si->setStatePropagator(boost::bind(&propagateState, _1, _2, _3, _4)));
+    bounds.setLow(-200);
+    bounds.setHigh(200);
+    stateSpace->setVolumeBounds(bounds);
     
-    oc::ControlSpacePtr cspace(new oc::RealVectorControlSpace(space, 2));
-    ob::RealVectorBounds cbounds(2);
-    cbounds.setLow(-1);
-    cbounds.setHigh(1);
-    cspace->as<oc::RealVectorControlSpace>()->setBounds(cbounds);
-
-    //oc::SpaceInformationPtr csi(new oc::SpaceInformation(space));
-    //csi->setMinMaxControlDuration(1, 5);
-
-    oc::SimpleSetup ss(cspace);
-    //ss.setStateValidityChecker(boost::bind(&isStateValid, ss.getSpaceInformation().get(), _1));
-    //ss.setStatePropagator(boost::bind(&propagateState, _1, _2, _3, _4));
+    bounds.setLow(-20);
+    bounds.setHigh(20);
+    stateSpace->setLinearVelocityBounds(bounds);
+    stateSpace->setAngularVelocityBounds(bounds);
     
-    ob::ScopedState<> start(space);
-    start.random();
-
-    ob::ScopedState<> goal(space);
-    goal.random();
-
-    //ob::ProblemDefinitionPtr pdef(new ob::ProblemDefinition(si));
-    //pdef->setStartAndGoalStates(start, goal);
-
-    //ob::PlannerPtr planner(new og::RRTConnect(si));
-    //planner->setProblemDefinition(pdef);
-    //planner->setup();
-
-    //ob::PlannerStatus solved = planner->solve(1.0);
-    
-    ss.setStartAndGoalStates(start, goal);
     ss.setup();
-    ob::PlannerStatus solved = ss.solve(100.0);
-
-    if (solved)
-    {
-        // get the goal representation from the problem definition (not the same as the goal state)
-        // and inquire about the found path
-        og::PathGeometric path = ss.getSolutionPath().asGeometric();
-        std::cout << "Found solution:" << std::endl;
-        // print the path to screen
-        path.printAsMatrix(std::cout);
-    }
+    ss.print();
+    
+    if (ss.solve(10))
+        ss.getSolutionPath().asGeometric().printAsMatrix(std::cout);
 }
 
-#if 0
-int main(int argc, const char * argv[]) {
-    plan();
-    return 0;
-}
-#endif
 
+
+
+
+
+
+
+/// @cond IGNORE
+
+/***********************************************
+ * Member function implementations             *
+ ***********************************************/
+
+void RigidBodyEnvironment::createWorld(void) {
+    // BEGIN SETTING UP AN OPENDE ENVIRONMENT
+    // ***********************************
+    
+    bodyWorld = dWorldCreate();
+    space = dHashSpaceCreate(0);
+    
+    dWorldSetGravity(bodyWorld, 0, 0, -0.981);
+    
+    double lx = 0.2;
+    double ly = 0.2;
+    double lz = 0.1;
+    
+    dMassSetBox(&m, 1, lx, ly, lz);
+    
+    boxGeom = dCreateBox(space, lx, ly, lz);
+    boxBody = dBodyCreate(bodyWorld);
+    dBodySetMass(boxBody, &m);
+    dGeomSetBody(boxGeom, boxBody);
+    
+    // *********************************
+    // END SETTING UP AN OPENDE ENVIRONMENT
+    
+    setPlanningParameters();
+}
+
+void RigidBodyEnvironment::destroyWorld() {
+    dSpaceDestroy(space);
+    dWorldDestroy(bodyWorld);
+}
+
+void RigidBodyEnvironment::setPlanningParameters() {
+    // Fill in parameters for OMPL:
+    world_ = bodyWorld;
+    collisionSpaces_.push_back(space);
+    stateBodies_.push_back(boxBody);
+    stepSize_ = 0.05;
+    maxContacts_ = 3;
+    minControlSteps_ = 10;
+    maxControlSteps_ = 500;
+}
--- planner.h
+++ planner.h
@@ -9,17 +9,7 @@
 #ifndef PLANNER_H_INCLUDED
 #define PLANNER_H_INCLUDED
 
-#include <ompl/base/StateSpace.h>
-#include <ompl/base/SpaceInformation.h>
-#include <ompl/base/ProblemDefinition.h>
-#include <ompl/control/ControlSpace.h>
-#include <ompl/control/ControlSpaceTypes.h>
-#include <ompl/control/SimpleSetup.h>
-#include <ompl/control/SpaceInformation.h>
-#include <ompl/control/spaces/RealVectorControlSpace.h>
-#include <ompl/geometric/SimpleSetup.h>
-#include <ompl/base/spaces/SE3StateSpace.h>
-#include <ompl/geometric/planners/rrt/RRTConnect.h>
+void plan();
 
 #endif // PLANNER_H_INCLUDED
 
