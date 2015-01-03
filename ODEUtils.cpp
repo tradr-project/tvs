e7743d9382334a29ee5070caebb8ad8e0b50bbb1
Federico Ferri <federico.ferri.it@gmail.com>
2015-01-03 23:55:35 +0100

    fix an arror in dRigidBodyArraySetRotation; add functions for setting linear/angular velocity of a rigid body array

--- ODEUtils.cpp
+++ ODEUtils.cpp
@@ -28,20 +28,49 @@ void dRigidBodyArraySetRotation(dBodyID *bodyArray, size_t arraySize, dBodyID ce
     dMULTIPLY0_333(R0Rs, R0, Rs);
     dMatrix3 R0RsR0t;
     dMULTIPLY2_333(R0RsR0t, R0Rs, R0);
-    dVector3 R0Rsp0;
-    dMULTIPLY0_331(R0Rsp0, R0Rs, p0);
     for(size_t i = 0; i < arraySize; i++) {
         const dReal *pi = dBodyGetPosition(bodyArray[i]);
         const dReal *Ri = dBodyGetRotation(bodyArray[i]);
         dMatrix3 R1;
         dMULTIPLY0_333(R1, R0RsR0t, Ri);
-        dVector3 p1, R0RsR0tpi, R0RsR0tpi_R0Rsp0;
-        dMULTIPLY0_331(R0RsR0tpi, R0RsR0t, pi);
-        dOP(R0RsR0tpi_R0Rsp0, -, R0RsR0tpi, R0Rsp0);
-        dOP(p1, +, R0RsR0tpi_R0Rsp0, p0);
+        dVector3 p1, pi_p0, R0RsR0t__pi_p0;
+        dOP(pi_p0, -, pi, p0);
+        dMULTIPLY0_331(R0RsR0t__pi_p0, R0RsR0t, pi_p0);
+        dOP(p1, +, R0RsR0t__pi_p0, p0);
         dBodySetPosition(bodyArray[i], p1[0], p1[1], p1[2]);
         dBodySetRotation(bodyArray[i], R1);
     }
+}
+
+void dRigidBodyArraySetLinearVel(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal lx, dReal ly, dReal lz) {
+    for(size_t i = 0; i < arraySize; i++) {
+        dBodySetLinearVel(bodyArray[i], lx, ly, lz);
+    }
+}
+
+void dRigidBodyArraySetAngularVel(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal ax, dReal ay, dReal az) {
+    const dReal *p0 = dBodyGetPosition(center);
+    dVector3 omega = {ax, ay, az};
+    for(size_t i = 0; i < arraySize; i++) {
+        const dReal *p = dBodyGetPosition(bodyArray[i]);
+        dVector3 pdot, r;
+        dOP(r, -, p, p0);
+        dCalcVectorCross3(pdot, omega, r);
+        dBodySetLinearVel(bodyArray[i], pdot[0], pdot[1], pdot[2]);
+        dBodySetAngularVel(bodyArray[i], ax, ay, az);
+    }
+}
+
+void dRigidBodyArrayAddLinearVel(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal lx, dReal ly, dReal lz) {
+    for(size_t i = 0; i < arraySize; i++) {
+        const dReal *v = dBodyGetLinearVel(bodyArray[i]);
+        dBodySetLinearVel(bodyArray[i], v[0] + lx, v[1] + ly, v[2] + lz);
+    }
+}
+
+void dRigidBodyArraySetVel(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal lx, dReal ly, dReal lz, dReal ax, dReal ay, dReal az) {
+    dRigidBodyArraySetAngularVel(bodyArray, arraySize, center, ax, ay, az);
+    dRigidBodyArrayAddLinearVel(bodyArray, arraySize, center, lx, ly, lz);
 }
 
 const char * dClassGetName(int c) {
--- ODEUtils.h
+++ ODEUtils.h
@@ -13,6 +13,10 @@
 
 void dRigidBodyArraySetPosition(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal x, dReal y, dReal z);
 void dRigidBodyArraySetRotation(dBodyID *bodyArray, size_t arraySize, dBodyID center, const dReal *Rs);
+void dRigidBodyArraySetLinearVel(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal lx, dReal ly, dReal lz);
+void dRigidBodyArraySetAngularVel(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal ax, dReal ay, dReal az);
+void dRigidBodyArrayAddLinearVel(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal lx, dReal ly, dReal lz);
+void dRigidBodyArraySetVel(dBodyID *bodyArray, size_t arraySize, dBodyID center, dReal lx, dReal ly, dReal lz, dReal ax, dReal ay, dReal az);
 const char * dClassGetName(int c);
 
 #endif /* defined(__tvs__ODEUtils__) */
