c4aa80935d43e9a45e426c0c18fa30ac68acfb75
Federico Ferri <federico.ferri.it@gmail.com>
2015-01-18 10:43:06 +0100

    add linear profile integrator

--- utils.cpp
+++ utils.cpp
@@ -2,6 +2,11 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <sstream>
 #include <string>
+#include "utils.h"
+
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
 
 std::string getDateTimeString(const char *format) {
     const boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
@@ -10,4 +15,25 @@ std::string getDateTimeString(const char *format) {
     ss.imbue(std::locale(ss.getloc(),f));
     ss << now;
     return ss.str();
+}
+
+void LinVelProfInt::step(double timeStep) {
+    x += double(sgn(tx - x)) * vmax * timeStep;
+}
+
+double LinVelProfInt::get() {
+    return x;
+}
+
+void LinVelProfInt::reset() {
+    tx = 0;
+    x = 0;
+}
+
+void LinVelProfInt::set(double target) {
+    tx = target;
+}
+
+void LinVelProfInt::setSlope(double slope) {
+    vmax = slope;
 }
--- utils.h
+++ utils.h
@@ -9,6 +9,20 @@
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
 
+template <typename T> int sgn(T val);
+
 std::string getDateTimeString(const char *format = "%Y%m%d%H%M%S");
+
+// linear profile integrator
+class LinVelProfInt {
+private:
+    double x, tx, vmax;
+public:
+    void step(double timeStep);
+    double get();
+    void reset();
+    void set(double target);
+    void setSlope(double slope);
+};
 
 #endif // UTILS_H_INCLUDED
