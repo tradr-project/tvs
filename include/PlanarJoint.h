79fb44c499504bc9a21766e08b984162535a9827
Martin Pecka <peckama2@fel.cvut.cz>
2016-01-20 20:36:45 +0100

    Made the PlanarJoint math more sound. Documented it.

--- include/PlanarJoint.h
+++ include/PlanarJoint.h
@@ -1,60 +1,122 @@
-/*************************************************************************
- *                                                                       *
- * Open Dynamics Engine, Copyright (C) 2001,2002 Russell L. Smith.       *
- * All rights reserved.  Email: russ@q12.org   Web: www.q12.org          *
- *                                                                       *
- * This library is free software; you can redistribute it and/or         *
- * modify it under the terms of EITHER:                                  *
- *   (1) The GNU Lesser General Public License as published by the Free  *
- *       Software Foundation; either version 2.1 of the License, or (at  *
- *       your option) any later version. The text of the GNU Lesser      *
- *       General Public License is included with this library in the     *
- *       file LICENSE.TXT.                                               *
- *   (2) The BSD-style license that is included with this library in     *
- *       the file LICENSE-BSD.TXT.                                       *
- *                                                                       *
- * This library is distributed in the hope that it will be useful,       *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the files    *
- * LICENSE.TXT and LICENSE-BSD.TXT for more details.                     *
- *                                                                       *
- *************************************************************************/
-
 #ifndef _ODE_PLANAR_JOINT_H_
 #define _ODE_PLANAR_JOINT_H_
 
+// HACK start
+// There is no standard way to develop new joints without actually forking the whole ODE.
+// As this seems like too much overhead to me, instead I just place ODE sources to ../ode, and use the non-public API
+// needed for developing new joints.
+
+// For whatever reason, the uint8 type needs to be declared manually.
 #include <stdint.h>
 typedef uint8_t uint8;
 
-#include "../../ode/ode/src/joints/joint.h"
+#include "joints/joint.h" // this has to be directed to ode sources directory!
+// HACK end
 
 
-// 2d joint
-
+/**
+ * @brief A joint that restricts movement of a body to a given (arbitrary) 2D plane.
+ *
+ * The restricting plane may be either static (connected to the world) or may move along with another body.
+ */
 struct dxPlanarJoint : public dxJoint
 {
 public:
+    /** The anchor point in global coordinates at the time it has been set. */
     dVector3 anchor;
+    /** The plane normal in global coordinates at the time it has been set. */
     dVector3 planeNormal;
 
+    /**
+     * Create the planar joint with default restriction to the @c z=0 plane.
+     *
+     * @param world The world to create the joint in.
+     */
+    dxPlanarJoint( dxWorld *world);
 
-    dxPlanarJoint( dxWorld *w );
+    ///////////////////////////////
+    // dxJoint API implementation
+    ///////////////////////////////
+
+    /** @copydoc dxJoint::getSureMaxInfo() */
     virtual void getSureMaxInfo( SureMaxInfo* info );
+
+    /** @copydoc dxJoint::getInfo1() */
     virtual void getInfo1( Info1* info );
+
+    /** @copydoc dxJoint::getInfo2() */
     virtual void getInfo2( dReal worldFPS, dReal worldERP, const Info2Descr* info );
+
+    /** @copydoc dxJoint::type() */
     virtual dJointType type() const;
+
+    /** @copydoc dxJoint::size() */
     virtual size_t size() const;
 
+    ///////////////////
+    // Custom methods
+    ///////////////////
+
+    /**
+     * @brief Update the body1 and body2 local restricting plane equations based on @c anchor, @c planeNormal and
+     *        current poses of the bodies.
+     *
+     * Called automatically after dJointPlanarSetPlaneNormal() and dJointPlanarSetAnchor().
+     */
     virtual void updatePlane();
 
-public:
-    dVector3 axis1, axis2;
-    dVector3 anchor1, anchor2;
+protected:
+    dVector3    axis1, /**< The restricting plane normal in body1 local coordinates. */
+                axis2, /**< The restricting plane normal in body2 local coordinates. */
+                anchor1, /**< The restricting plane anchor point in body1 local coordinates. */
+                anchor2; /**< The restricting plane anchor point in body2 local coordinates. */
 
 };
 
-ODE_API dJointID dJointCreatePlanar (dWorldID w, dJointGroupID group); // body 2 (or world) moves the plane, whereas body 1 restricts its movement to the plane
+/**
+ * @brief Create a joint that restricts movement of a body to a given (arbitrary) 2D plane (defined by an anchor
+ * ("origin") and plane normal).
+ *
+ * The restricting plane may be either static (connected to the world) or may move along with another body.
+ *
+ * When attaching, body2 (or world) moves the plane, whereas body1's movement is restricted by the plane.
+ *
+ * After bodies are attached to the joint, call dJointPlanarSetPlaneNormal() and dJointPlanarSetAnchor() to set the
+ * restricting plane. It will remember the relative pose to both bodies at the moment it was called, and this relative
+ * pose will be used to constrain body1 motion. By default, body1 is restricted to move along the @c z=0 plane.
+ *
+ * @ingroup joints
+ *
+ * @param world The world to create the joint in.
+ * @param group Set to 0 to allocate the joint normally.
+ *              If it is nonzero the joint is allocated in the given joint group.
+ *
+ * @return The joint.
+ */
+ODE_API dJointID dJointCreatePlanar (dWorldID world, dJointGroupID group);
+
+/**
+ * @brief Set the anchor point of the plane restricting a planar joint.
+ *
+ * This is one of the points lying in the pla. Together with the plane normal, it uniquely defines the plane.
+ *
+ * @ingroup joints
+ *
+ * @param joint The joint for which to set the anchor.
+ * @param anchor The anchor point in global coordinates.
+ */
 ODE_API void dJointPlanarSetAnchor(dJointID joint, dVector3 anchor);
+
+/**
+ * @brief Set the normal of the plane restricting a planar joint.
+ *
+ * Together with the anchor point, it uniquely defines the plane.
+ *
+ * @ingroup joints
+ *
+ * @param joint The joint for which to set the plane normal.
+ * @param anchor The plane normal in global coordinates (not necessarily unit-length).
+ */
 ODE_API void dJointPlanarSetPlaneNormal(dJointID joint, dVector3 planeNormal);
 
 #endif
--- src/PlanarJoint.cpp
+++ src/PlanarJoint.cpp
@@ -9,187 +9,177 @@
 #define checktype(j,t) dUASSERT(j->type() == dJointType##t, \
     "joint type is not " #t)
 
+dJointID dJointCreatePlanar(dWorldID world, dJointGroupID group) {
+    dAASSERT (world);
 
-//****************************************************************************
-// PlanarJoint
-//****************************************************************************
-
-dJointID dJointCreatePlanar(dWorldID w, dJointGroupID group) {
-    dAASSERT (w);
-
-    dxJoint *j;
+    dxJoint *joint;
     if (group) {
-        j = group->alloc<dxPlanarJoint>(w);
+        joint = group->alloc<dxPlanarJoint>(world);
     } else {
-        j = new dxPlanarJoint(w);
+        joint = new dxPlanarJoint(world);
     }
-    return j;
+    return joint;
 }
 
 
-dxPlanarJoint::dxPlanarJoint( dxWorld *w ) :
-        dxJoint( w )
+dxPlanarJoint::dxPlanarJoint(dxWorld *world) :
+        dxJoint(world)
 {
-    dSetZero(anchor, 4);
+    dSetZero(anchor, 3);
+    dCopyVector3(anchor1, anchor);
+    dCopyVector3(anchor2, anchor);
 
     // default to plane z=0
-    dSetZero(planeNormal, 4);
+    dSetZero(planeNormal, 3);
     planeNormal[2] = 1;
-
-    dSetZero(axis1, 4);
-    dSetZero(axis2, 4);
-    dSetZero(anchor1, 4);
-    dSetZero(anchor2, 4);
+    dCopyVector3(axis1, planeNormal);
+    dCopyVector3(axis2, planeNormal);
 }
 
 
-void
-dxPlanarJoint::getSureMaxInfo( SureMaxInfo* info )
+void dxPlanarJoint::getSureMaxInfo(SureMaxInfo* info)
 {
     info->max_m = 3;
 }
 
 
-void
-dxPlanarJoint::getInfo1( dxJoint::Info1 *info )
+void dxPlanarJoint::getInfo1(dxJoint::Info1* info)
 {
     info->nub = 3;
     info->m = 3;
 }
 
-void
-dxPlanarJoint::getInfo2( dReal worldFPS, dReal worldERP, const Info2Descr *info )
+void dxPlanarJoint::getInfo2(dReal worldFPS, dReal worldERP, const Info2Descr* info)
 {
-    int r0 = 0;
-    int r1 = info->rowskip;
-    int r2 = 2 * r1;
-    dReal eps = worldFPS * worldERP;
+    /* ====================================================================
+     * This joint restricts 2 rotational and 1 translational DOF.
+     *
+     * The 2 rotational constraints are essentially the same as in dxJointPiston, so the implementation here is mostly
+     * just a copy of them.
+     *
+     * The translational 1DOF constraint has not yet been implemented for any joint in ODE, so we'll talk more about it.
+     * It is similar to the constraints in a slider joint, but in this case we only want body2 to move the plane, and
+     * body1 anchor has no use in this case, too.
+     *
+     * We basically want to express the plane in body2 local coordinates (because it should move along with body2), and
+     * check, if body1 origin lies in the plane.
+     *
+     * Let's say n' is the plane normal in body2 coordinates and a' is the anchor point in body2 coordinates
+     * (with n, a, being the corresponding global variables).
+     * Further, let's call the global position of body1 p1, and similarly p2 for body2.
+     * Last, let's call body2 rotation matrix R2 (so that n = R2*n' and a = R2*a' + p2).
+     *
+     * The plane can then be expressed in global coordinates as:
+     * (n^ . x) - (n^ . a) = 0,
+     * where x is in global coordinates and ^ denotes vector/matrix transpose.
+     *
+     * Rewriting the equation using body2 local coordinates and setting x=p1, we get the constraint:
+     * (R2*n')^ * p1 - (R2*n')^ * (R2*a' + p2) = 0
+     * ...
+     * (n'^ * R2^ * p1) - (n'^ * R2^ * p2) - (n'^ * a') = 0
+     * (n^ * p1) - (n^ * p2) - (n'^ * a') = 0
+     *
+     * The part left to "=" will be the "c" on the RHS of the Jacobian equation (because it expresses
+     * the distance of p1 from the plane).
+     *
+     * Next, we need to take time derivative of the constraint to get the J1 and J2 parts.
+     * v1, v2, w1 and w2 denote the linear and angular velocities od body1 and body2.
+     * [a]x denotes the skew-symmetric cross-product matrix of vector a.
+     *
+     * d/dt [(n'^ * R2^ * p1) - (n'^ * R2^ * p2) - (n'^ * a') = 0]        (n', a' are constant in time)
+     * n'^ * ((d/dt[R2^] * p1) + (R2^ * v1)) - n'^ * ((d/dt[R2^] * p2) + (R2^ * v2)) = 0
+     * n'^ * ((([w2]x R2)^ * p1) + (R2^ * v1)) - n'^ * ((([w2]x R2)^ * p2) + (R2^ * v2)) = 0
+     * ...
+     * n^*v1 - n^*v2 + [n]x (p1 - p2) . w2 = 0
+     * -n^*v1 + n^*v2 + [n]x (p2 - p1) . w2 = 0
+     *
+     * Thus we see that
+     * J1l = -n
+     * J2l = n
+     * J1a = 0
+     * J2a = [n]x (p2 - p1)
+     * c = eps * ( (n^ * p1) - (n^ * a) )
+     *
+     */
 
-    // ======================================================================
-    // Work on the angular part (i.e. row 0, 1)
-    // Set the two orientation rows. The rotoide axis should be the only
-    // unconstrained rotational axis, the angular velocity of the two bodies
-    // perpendicular to the rotoide axis should be equal.
-    // Thus the constraint equations are:
-    //    p*w1 - p*w2 = 0
-    //    q*w1 - q*w2 = 0
-    // where p and q are unit vectors normal to the rotoide axis, and w1 and w2
-    // are the angular velocity vectors of the two bodies.
-    // Since the rotoide axis is the same as the prismatic axis.
-    //
-    //
-    // Also, compute the right hand side (RHS) of the rotation constraint equation set.
-    // The first 2 element will result in the relative angular velocity of the two
-    // bodies along axis p and q. This is set to bring the rotoide back into alignment.
-    // if `theta' is the angle between ax1 and ax2, we need an angular velocity
-    // along u to cover angle erp*theta in one step :
-    //   |angular_velocity| = angle/time = erp*theta / stepsize
-    //                      = (erp*fps) * theta
-    //    angular_velocity  = |angular_velocity| * u
-    //                      = (erp*fps) * theta * u
-    // where rotation along unit length axis u by theta brings body 2's frame
-    //
-    // if theta is smallish, sin(theta) ~= theta and cos(theta) ~= 1
-    // where the quaternion of the relative rotation between the two bodies is
-    //    quat = [cos(theta/2) sin(theta/2)*u]
-    //    quat = [1 theta/2*u]
-    //         => q[0] ~= 1
-    //            2 * q[1+i] = theta * u[i]
-    //
-    // Since there is no constraint along the rotoide axis
-    // only along p and q that we want the same angular velocity and need to reduce
-    // the error
+    const int row0Index = 0;
+    const int row1Index = info->rowskip;
+    const int row2Index = 2 * row1Index;
+    const dReal eps = worldFPS * worldERP;
 
-    dVector3 ax1;
+    dVector3 globalAxis1;
+    dBodyVectorToWorld(node[0].body, axis1[0], axis1[1], axis1[2], globalAxis1);
+
+    dVector3 globalAxis2;
+    dVector3 globalAnchor;
+
     if (node[1].body) {
-        dBodyVectorToWorld(node[1].body, axis2[0], axis2[1], axis2[2], ax1);
+        dBodyVectorToWorld(node[1].body, axis2[0], axis2[1], axis2[2], globalAxis2);
+        dBodyGetRelPointPos(node[1].body, anchor2[0], anchor2[1], anchor2[2], globalAnchor);
     } else {
-        dCopyVector3(ax1, axis2);
+        dCopyVector3(globalAxis2, axis2);
+        dCopyVector3(globalAnchor, anchor2);
     }
+
+    ///////////////////////////////////////////////////////
+    // Angular velocity constraints (2 rows)
+    //
+    // Refer to the piston joint source code for details.
+    ///////////////////////////////////////////////////////
 
     // Find the 2 direction vectors of the plane.
     dVector3 p, q;
-    dPlaneSpace ( ax1, p, q );
+    dPlaneSpace ( globalAxis2, p, q );
 
-    // LHS 2
-    dCopyVector3 ( ( info->J2a ) + r0, p );
-    dCopyVector3 ( ( info->J2a ) + r1, q );
+    // LHS 0
+    dCopyNegatedVector3 (info->J1a + row0Index, p );
+    dCopyNegatedVector3 (info->J1a + row1Index, q );
 
     // LHS 1
-    dCopyNegatedVector3 ( ( info->J1a ) + r0, p );
-    dCopyNegatedVector3 ( ( info->J1a ) + r1, q );
+    dCopyVector3 (info->J2a + row0Index, p );
+    dCopyVector3 (info->J2a + row1Index, q );
 
-
-    // Some math for the RHS
-    dVector3 ax2;
-    dBodyVectorToWorld(node[0].body, axis1[0], axis1[1], axis1[2], ax2);
-
+    // RHS 0 & 1 (absolute errors of the axis direction computed by dot product of the desired and actual axis)
     dVector3 b;
-    dCalcVectorCross3( b, ax1, ax2 );
-
-    // RHS
+    dCalcVectorCross3( b, globalAxis2, globalAxis1 );
     info->c[0] = eps * dCalcVectorDot3 ( p, b );
     info->c[1] = eps * dCalcVectorDot3 ( q, b );
 
-    // linear velocity constraint (removes 1 translational DOF along the plane normal)
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // Linear velocity constraint (1 row, removes 1 translational DOF along the plane normal)
+    //
+    // Only body1 should be restricted by the plane, which is moved along with body2.
+    //////////////////////////////////////////////////////////////////////////////////////////
 
-    // node[0] je vazany na rovinu, rovina je vazana na node[1] (pripadne svet)
+    dVector3 body1Center;
+    dCopyVector3(body1Center, node[0].body->posr.pos);
 
-    dVector3 globalAxis;
+    dVector3 body2Center = {0, 0, 0};
     if (node[1].body) {
-        dBodyVectorToWorld(node[1].body, axis2[0], axis2[1], axis2[2], globalAxis);
-    } else {
-        dCopyVector3(globalAxis, axis2);
+        dCopyVector3(body2Center, node[1].body->posr.pos);
     }
 
-    dCopyNegatedVector3(info->J1l + r2, globalAxis);
-    dCopyVector3(info->J2l + r2, globalAxis);
+    // LHS 2
+    dCopyNegatedVector3(info->J1l + row2Index, globalAxis2);
+    dCopyVector3(       info->J2l + row2Index, globalAxis2);
 
+    dVector3 body2_body1;
+    dSubtractVectors3(body2_body1, body2Center, body1Center);
+    dCalcVectorCross3(info->J2a + row2Index, globalAxis2, body2_body1);
 
-    dVector3 pos1;
-    dCopyVector3(pos1, node[0].body->posr.pos);
-
-    dVector3 pos2 = {0, 0, 0};
-    if (node[1].body) {
-        dCopyVector3(pos2, node[1].body->posr.pos);
-    }
-
-    dVector3 pos2_pos1;
-    dSubtractVectors3(pos2_pos1, pos2, pos1);
-
-    dVector3 pos2_pos1LocalToBody2;
-    if (node[1].body) {
-        dBodyVectorFromWorld(node[1].body, pos2_pos1[0], pos2_pos1[1], pos2_pos1[2], pos2_pos1LocalToBody2);
-    } else {
-        dCopyVector3(pos2_pos1LocalToBody2, pos2_pos1);
-    }
-
-    dCalcVectorCross3(info->J1a + r2, axis1, pos2_pos1LocalToBody2);
-
-    // RHS
-
-    dReal t1, t2, t3;
-    t1 = dCalcVectorDot3(globalAxis, pos1);
-    t2 = dCalcVectorDot3(axis2, anchor2);
-    t3 = dCalcVectorDot3(globalAxis, pos2);
-
-    info->c[2] = eps * (t1 - t2 - t3);
-
-    if (t1 - t2 - t3 > 1) {
-        std::cout << (t1 - t2 - t3) << std::endl;
-    }
+    // RHS 2 (distance of body1 center from the plane)
+    info->c[2] = eps * (dCalcVectorDot3(globalAxis2, body1Center) - dCalcVectorDot3(globalAxis2, globalAnchor));
 }
 
 
-dJointType
-dxPlanarJoint::type() const
+// HACK since we cannot effectively create a new joint type, we misuse the Plane2D joint type.
+dJointType dxPlanarJoint::type() const
 {
     return dJointTypePlane2D;
 }
 
 
-size_t
-dxPlanarJoint::size() const
+size_t dxPlanarJoint::size() const
 {
     return sizeof( *this );
 }
@@ -197,26 +187,21 @@ dxPlanarJoint::size() const
 void dJointPlanarSetAnchor(dJointID joint, dVector3 anchor) {
     dUASSERT( joint, "bad joint argument" );
     checktype( joint, Plane2D );
-    dxPlanarJoint* planarJoint = ( dxPlanarJoint* )( joint );
+    dxPlanarJoint* planarJoint = (dxPlanarJoint*) joint;
 
-    planarJoint->anchor[0] = anchor[0];
-    planarJoint->anchor[1] = anchor[1];
-    planarJoint->anchor[2] = anchor[2];
+    dCopyVector3(planarJoint->anchor, anchor);
 
     planarJoint->updatePlane();
 }
 
 void dJointPlanarSetPlaneNormal(dJointID joint, dVector3 planeNormal) {
     dUASSERT( joint, "bad joint argument" );
+    checktype( joint, Plane2D );
+    dxPlanarJoint* planarJoint = (dxPlanarJoint*) joint;
+
     dUASSERT(dLENGTHSQUARED(planeNormal) > 0.000001, "plane normal cannot have zero length");
 
-    checktype( joint, Plane2D );
-    dxPlanarJoint* planarJoint = ( dxPlanarJoint* )( joint );
-
-    planarJoint->planeNormal[0] = planeNormal[0];
-    planarJoint->planeNormal[1] = planeNormal[1];
-    planarJoint->planeNormal[2] = planeNormal[2];
-
+    dCopyVector3(planarJoint->planeNormal, planeNormal);
     dNormalize3(planarJoint->planeNormal);
 
     planarJoint->updatePlane();
@@ -225,21 +210,23 @@ void dJointPlanarSetPlaneNormal(dJointID joint, dVector3 planeNormal) {
 
 void dxPlanarJoint::updatePlane() {
 
-    dBodyID b0 = this->node[0].body;
-    if ( b0 )
+    dBodyID body1 = this->node[0].body;
+    if (body1)
     {
+        // compute plane normal and anchor coordinates in the local coordinates of the first body
+        dBodyVectorFromWorld(body1, planeNormal[0], planeNormal[1], planeNormal[2], axis1);
+        dBodyGetPosRelPoint(body1, anchor[0], anchor[1], anchor[2], anchor1);
 
-        dBodyVectorFromWorld(b0, planeNormal[0], planeNormal[1], planeNormal[2], axis1);
-        dBodyGetPosRelPoint(b0, anchor[0], anchor[1], anchor[2], anchor1);
-
-        dBodyID b1 = this->node[1].body;
-        if ( b1 )
+        dBodyID body2 = this->node[1].body;
+        if (body2) // second body given, attach the plane to it
         {
-            dBodyVectorFromWorld(b1, planeNormal[0], planeNormal[1], planeNormal[2], axis2);
-            dBodyGetPosRelPoint(b1, anchor[0], anchor[1], anchor[2], anchor2);
+            // compute plane normal and anchor coordinates in the local coordinates of the second body
+            dBodyVectorFromWorld(body2, planeNormal[0], planeNormal[1], planeNormal[2], axis2);
+            dBodyGetPosRelPoint(body2, anchor[0], anchor[1], anchor[2], anchor2);
         }
-        else
+        else // second body not given, attach the plane to the world
         {
+            // plane normal and anchor coordinates in the world world frame are the same as global coordinates
             dCopyVector3(axis2, planeNormal);
             dCopyVector3(anchor2, anchor);
         }
